91694320610c3f9c9cfef10fc454102338b7ff36
John Huang <ymhuang@fmbase.tw>
2020-03-26 17:03:02 +0800

    Add DiscriminatorArrayValidatorTest class for unit testing

---
+++ tests/DiscriminatorArrayValidatorTest.php
@@ -0,0 +1,174 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Mmal\OpenapiValidator\Tests;
+
+use Mmal\OpenapiValidator\Validator;
+use PHPUnit\Framework\TestCase;
+
+class DiscriminatorArrayValidatorTest extends BaseTestCase
+{
+    protected function getTestedClass(): Validator
+    {
+        $schema = file_get_contents(__DIR__.'/specs/discriminator-array-example-spec.yaml');
+        return $this->getInstance($schema);
+    }
+
+    public function testValidObject1()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooks', 200, [
+            [
+                'type' => 'obj1',
+                'tag' => 'foo',
+            ],
+            [
+                'type' => 'obj1',
+                'tag' => 'qoo',
+            ]
+        ]);
+
+        $this->assertFalse($error->hasErrors());
+    }
+
+    public function testValidObject2()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooks', 200, [
+            [
+                'type' => 'obj2',
+                'name' => 'foo',
+            ],
+            [
+                'type' => 'obj2',
+                'name' => 'qoo',
+            ],
+            [
+                'type' => 'obj2',
+                'name' => 'qoo',
+            ]
+        ]);
+
+        $this->assertFalse($error->hasErrors());
+    }
+
+    public function testInvalidTypeDoesNotMatch()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooks', 200, [
+            [
+                'type' => 'obj2',
+                'tag' => 'foo',
+            ],
+            [
+                'type' => 'obj2',
+                'tag' => 'foo',
+            ],
+            [
+                'type' => 'obj2',
+                'tag' => 'foo',
+            ]
+        ]);
+
+        $this->assertTrue($error->hasErrors());
+    }
+
+    public function testInvalidTypeDoesNotMatch2()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooks', 200, [
+            [
+                'type' => 'obj1',
+                'name' => 'foo',
+            ],
+            [
+                'type' => 'obj1',
+                'name' => 'foo',
+            ]
+        ]);
+
+        $this->assertTrue($error->hasErrors());
+    }
+
+    public function testValidNestedDiscrimanted()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooksNested', 200, [
+            [
+                'class' => 'foo',
+                'subObj' => [
+                    'type' => 'obj1',
+                    'tag' => 'foo',
+                ]
+            ],
+            [
+                'class' => 'foo',
+                'subObj' => [
+                    'type' => 'obj1',
+                    'tag' => 'foo',
+                ]
+            ],
+            [
+                'class' => 'foo',
+                'subObj' => [
+                    'type' => 'obj1',
+                    'tag' => 'foo',
+                ]
+            ]
+        ]);
+
+        $this->assertFalse($error->hasErrors());
+    }
+
+    public function testInValidNestedDiscrimanted()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooksNested', 200, [
+            [
+                'class' => 'foo',
+                'subObj' => [
+                    'type' => 'obj1',
+                    'tag' => 4
+                ]
+            ],
+            [
+                'class' => 'foo',
+                'subObj' => [
+                    'type' => 'obj1',
+                    'tag' => 4
+                ]
+            ]
+        ]);
+
+        $this->assertTrue($error->hasErrors());
+    }
+
+    public function testValidNestedDiscrimantedOtherObject()
+    {
+        $validator = $this->getTestedClass();
+
+        $error = $validator->validate('getBooksNested', 200, [
+            [
+                'class' => 'bar',
+                'count' => 4
+            ],
+            [
+                'class' => 'bar',
+                'count' => 4
+            ],
+            [
+                'class' => 'bar',
+                'count' => 4
+            ]
+        ]);
+
+        $this->assertFalse($error->hasErrors());
+    }
+}
